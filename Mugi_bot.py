53bc4d61e6444889cbc6703429ecd90799fadb13
tmdorny <tmdorny@gmail.com>
2017-06-01 05:19:59 -0600

    mugi bot transendence

--- Mugi_bot.py
+++ Mugi_bot.py
@@ -14,6 +14,19 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
+    await bot.change_presence(game=discord.Game(name='Why'))
+
+bot.remove_command('help')
+
+#MUGI GIFS
+#
+#pillow throw 
+#
+#
+#
+#
+#
+
 
 @bot.command()
 async def add(left : int, right : int):
@@ -65,6 +78,50 @@ async def _bot():
 @bot.command(name='fish')
 async def fish():
 	""" posts the mugi fish gif """
-	await bot.say('https://media1.giphy.com/media/ezkeq9mDzGBuE/giphy.gif')
+	await bot.say('http://pa1.narvii.com/5795/1fc45ddac9245f5a3beff456383787db9138b336_hq.gif')
+
+@bot.command(name='fight')
+async def fight():
+	""" posts the mugi pillow gif """
+	await bot.say('https://media.tenor.com/images/396f1f445a1befa7b6e7b082a2fcbac8/tenor.gif')
+	await bot.say('ill fk u up bich')
+
+@bot.command(name='jam')
+async def jam():
+	""" posts the mugi jammin gif """
+	await bot.say('http://imgur.com/eR56w')
+
+@bot.command(name='amaze')
+async def amaze():
+	""" posts the mugi jammin gif """
+	await bot.say('https://media.tenor.com/images/571aa142a20ea4886ddf9b187094ea3c/tenor.gif')
+
+
+@bot.command(name='help')
+async def help():
+	await bot.say('if u need help u dumb as fk')
+
+@bot.event
+async def on_message(message):
+	"""parse test"""
+	print('dum')
+	if 'dum' in message.content and (message.author) != (bot.user): #str(message.author).startswith("Mugi"):
+		user = str(message.author)
+		name,id = user.split('#')
+		print(id)
+		phrase = message.author.mention + " you\'re dumb as hell tbh"
+		await bot.send_message(message.channel, phrase)
+
+	if message.tts:
+		await bot.send_message(message.channel, 'stfu dumb bitch')
+
+	if 'yosh' in message.content and (message.author) != (bot.user):
+		await bot.send_message(message.channel, 'http://i.imgur.com/oiZxHlZ.gif')
+
+	if 'fk u' in message.content and (message.author) != (bot.user):
+		await bot.send_message(message.channel, 'http://i.imgur.com/x0rE1OW.gif')
+		await bot.send_message(message.channel, 'O SHIT')
+
+	await bot.process_commands(message)
 
 bot.run('MzE5NzU0NTM5MjE1NDg2OTc3.DBFiNQ.O3nTIBSEIo6xS5riwmBNquvHxaQ')
