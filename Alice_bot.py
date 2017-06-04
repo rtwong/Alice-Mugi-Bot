6c956fb70df6444c6aeaaf1bfcf116d511737231
Randy Wong <rtwong@ualberta.ca>
2017-06-04 05:18:36 -0600

    Recipe and gifme functions

--- Alice_bot.py
+++ Alice_bot.py
@@ -2,6 +2,38 @@ import discord
 from discord.ext import commands
 import random
 import asyncio
+import aiohttp
+from bs4 import BeautifulSoup
+
+
+
+recipe_categories = ["chicken", "pizza", "cocktails", "pasta", "burgers", "sandwiches", "desserts", "salad"]
+recipe_urls = {
+	"chicken"    : "http://www.seriouseats.com/tags/recipes/chicken",
+	"pizza"      : "http://www.seriouseats.com/tags/recipes/pizza",
+	"cocktails"  : "http://www.seriouseats.com/recipes/topics/meal/drinks/cocktails",
+	"pasta"      : "http://www.seriouseats.com/tags/recipes/pasta",
+	"burgers"    : "http://www.seriouseats.com/tags/recipes/burger",
+	"sandwiches" : "http://www.seriouseats.com/sandwiches",
+	"desserts"   : "http://www.seriouseats.com/desserts",
+	"salad"      : "http://www.seriouseats.com/tags/recipes/salad"
+}
+
+alice_gifs = [
+"https://s-media-cache-ak0.pinimg.com/originals/78/f5/8e/78f58e593a193d470add8983973ee8c4.gif",
+"https://68.media.tumblr.com/de6d2ca14fc64f3b4bc73e3651bf5ab3/tumblr_nt5tg2WBcS1r60zuio1_500.gif",
+"https://s-media-cache-ak0.pinimg.com/originals/de/83/2d/de832de0340706a16d1b64321850f95e.gif",
+"http://pa1.narvii.com/5939/4d3eeb22212ff56d5e30b9b7596fa29d8dbad042_hq.gif",
+"https://media.giphy.com/media/13ZoPdc5aFQM5q/giphy.gif",
+"https://media.tenor.com/images/7735c516c3c0c5896f4c2cbd969c187e/tenor.gif",
+"http://pa1.narvii.com/6163/74711fb8f84cb1b93e04ef6c9d27995a72dc93cd_hq.gif",
+"http://pa1.narvii.com/6024/ee7acf9fccff51e7b8e05b9133e9ba9976053458_hq.gif",
+"http://i.imgur.com/iyARgYd.gif",
+"http://i.imgur.com/H0l6Mlb.gifv",
+"http://i43.photobucket.com/albums/e374/NeoRyo/JapaneseAnime/Shokugeki%20no%20Soma/Shokugeki%20no%20Soma%20-%20Episode%2013%20-%20Nakiri%20Alice%20teaches%20Ryou%20how%20to%20intimidate%20people.gif"
+]
+
+
 
 description = '''Alice Nakiri on duty!'''
 
@@ -28,15 +60,15 @@ async def on_message(message):
 
 
 @bot.command()
-async def wobwob():
+async def gifme():
 	"""Shows an enjoyable gif. """
-	await bot.say('http://imgur.com/QSoL1ge')
+	await bot.say(random.choice(alice_gifs))
 
 
-recipe_categories = ["chicken", "pizza", "cocktails", "pasta", "burgers", "sandwiches", "desserts", "salad"]
 @bot.command()
 async def recipe(request_category : str):
-	"""Links a recipe from SeriousEats of the specified type. """
+	"""Links a random recipe from SeriousEats. Recipe options are: chicken, pizza, cocktails, pasta, burgers, sandwiches, desserts, salad, or random
+	"""
 	request_category = request_category.lower()
 	if request_category == "random":
 		recipe_category = random.choice(recipe_categories)
@@ -45,7 +77,22 @@ async def recipe(request_category : str):
 	else:
 		await bot.say("Incorrect category, choose one of [chicken, pizza, cocktails, pasta, burgers, sandwiches, desserts, salad, random]")
 		return
-	print(recipe_category) 
+
+	async with aiohttp.get(recipe_urls[recipe_category]) as resp:
+		if resp.status == 200:
+			webpage = await resp.text()
+		else:
+			bot.say("File not found")
+
+
+	recipe_recommendations = []
+	soup = BeautifulSoup(webpage, "html.parser")
+	for link in soup.find_all('a', {"class" : "module__image-container module__link"}):
+		recipe_link = link.get('href')
+		if recipe_link != recipe_urls[recipe_category]:
+			recipe_recommendations.append(recipe_link)
+	await bot.say(random.choice(recipe_recommendations))
+
 
 
 
