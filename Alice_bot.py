f7b02aec9f2afb38ca7ef876ef2116077196d2cb
Randy Wong <rtwong@ualberta.ca>
2017-06-05 18:48:03 -0600

    Shokugeki gambling

--- Alice_bot.py
+++ Alice_bot.py
@@ -8,6 +8,10 @@ import sqlite3
 import os
 
 
+description = '''Alice Nakiri on duty!'''
+
+bot = commands.Bot(command_prefix='?', description=description, pm_help=True)
+
 
 recipe_categories = ["chicken", "pizza", "cocktails", "pasta", "burgers", "sandwiches", "desserts", "salad"]
 recipe_urls = {
@@ -35,14 +39,19 @@ alice_gifs = [
 "http://i43.photobucket.com/albums/e374/NeoRyo/JapaneseAnime/Shokugeki%20no%20Soma/Shokugeki%20no%20Soma%20-%20Episode%2013%20-%20Nakiri%20Alice%20teaches%20Ryou%20how%20to%20intimidate%20people.gif"
 ]
 
-
-
-description = '''Alice Nakiri on duty!'''
-
-bot = commands.Bot(command_prefix='?', description=description, pm_help=True)
+# tuples are (opponent name, win chance, payout, win gif, lose gif, win quote, lose quote)
+shokugeki_opponents = {
+	'joichiro_yukihira' : ("Joichiro Yukihira", 1, 100.0,"http://pa1.narvii.com/6021/915dc3a471a6966bd466ec002b4f79606f40f76c_hq.gif", "https://68.media.tumblr.com/527b2586255523f4f9221f97dfa794f3/tumblr_nuvpy2QwW61siue1no1_500.gif", "Glad you liked it!", "Isn't this nasty?"),
+	'alice_nakiri'      : ("Alice Nakiri", 10, 10.0, "http://68.media.tumblr.com/23ce70b3550a0301506c6e430b293600/tumblr_of78s8NjPf1v89ei5o1_500.gif", "https://media.tenor.com/images/f518febf0964959af19720ba1e759041/tenor.gif", "Winner!", "Wahhhh"),
+	'soma_yukihira'     : ("Soma Yukihira", 33, 3.0,"http://pa1.narvii.com/5764/1d00f4332957996f6d21be3b8eaae0416a081501_hq.gif","https://media.giphy.com/media/dYegLwRzhiY2Q/giphy.gif", "Glad you liked it!", "Try THIS!"),
+	'takumi_aldini'     : ("Takumi Aldini", 50, 2.0,"https://s-media-cache-ak0.pinimg.com/originals/96/a9/7c/96a97ce95e406147453bd77724445d14.gif","https://media.tenor.co/images/b116de827c21558bb2d4f67f85567f15/tenor.gif","ALDINIIIIIII", "I don't want to talk about it."),
+	'megumi_tadokoro'   : ("Megumi Tadokoro", 66, 1.5,"https://68.media.tumblr.com/0d1a2186d7f99e3646d8676418fae8b8/tumblr_of75ht79nB1v89ei5o1_500.gif","https://s-media-cache-ak0.pinimg.com/originals/c5/d2/50/c5d25025fd2de0ae4cfa359b0a7927d8.gif", "Thanks!", "Oh god oh god oh god."),
+	'kanichi_konishi'   : ("Kanichi Konishi", 90, 1.1, "https://68.media.tumblr.com/a1f57ba41ef084d3f4b64f18a380edbd/tumblr_nujeh76lqz1qj4q1zo1_500.gif", "https://cdn.discordapp.com/attachments/238032111125266432/321416685762510858/ezgif-3-b68ebfd882.gif", "I'M SO PROUD OF ME", "o")
+}
 
 member_points = {}
 db_filename = "AliceBotPoints.sqlite"
+
 
 
 async def point_counter():
@@ -142,6 +151,43 @@ async def bentos(ctx):
 		await bot.say("You currently have " + str(member_points[sender_id]) + " Bentos.")
 	else:
 		await bot.say("You currently have no Bentos, get cooking!")
+
+
+@bot.command(pass_context=True)
+async def shokugeki(ctx, points_bet : int):
+	""" Bento Gambling, random opponent, higher payout if given harder opponent"""
+	betting_id = ctx.message.author.id
+	member_points[betting_id] = member_points.get(betting_id,0)
+
+	if points_bet < 10:
+		await bot.say("You must stake 10 or more Bentos!")
+		return
+	if points_bet > member_points[betting_id]:
+		await bot.say("You do not have that many Bentos!")
+		return
+
+	member_points[betting_id] = member_points[betting_id] - points_bet
+	opponent = random.choice(list(shokugeki_opponents.items()))[1]
+	roll = random.randrange(1,100)
+	payout = int(points_bet*opponent[2])
+
+	if roll <= opponent[1]:
+		member_points[betting_id] = member_points[betting_id] + payout
+		await bot.say("You beat %s, winning %d Bentos!" % (opponent[0], payout - points_bet))
+		await bot.say("You currently have %d Bentos." % member_points[betting_id])
+		await bot.say(opponent[4])
+		await bot.say(opponent[6])
+	else:
+		await bot.say("You lose to %s, losing %d Bentos!" % (opponent[0], points_bet))
+		await bot.say("You currently have %d Bentos." % member_points[betting_id])
+		await bot.say(opponent[3])
+		await bot.say(opponent[5])
+
+	conn = sqlite3.connect(db_filename)
+	c = conn.cursor()
+	c.execute("INSERT or REPLACE into points_table (id, points) VALUES (%s, %s)" % (betting_id, member_points[betting_id]))
+	conn.commit()
+	conn.close()
 
 
 
